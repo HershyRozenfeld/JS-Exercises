ee10bacbad40b37b8a2c8b46051ec957c5718dd7
hershy rozenfeld <Y6764057@gmail.com>
2025-06-22 18:38:38 +0300

    Add collectorType function to count data types in an array

--- typeCollector.js
+++ typeCollector.js
@@ -1,3 +1,26 @@
 function collectorType(array){
-    
+    const typeCounts = {
+    number: 0,
+    string: 0,
+    object: 0,
+    boolean: 0,
+    null: 0
+  };
+  for (let item of array){
+    if(item === null){
+        typeCounts.null++;
+    } else {
+        let type = typeof item;
+        if (typeCounts.hasOwnProperty(type)){
+            typeCounts[type]++;
+        }
+    }
+  }
+  console.log(typeCounts);
 }
+
+const mixed = [
+  42, "hello", true, false, {}, [], null, "world", 0, null, { a: 1 }, 99
+];
+
+collectorType(mixed);
