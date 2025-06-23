636f315df6d9867632fa77bc87dc7e4ba46d5b78
hershy rozenfeld <Y6764057@gmail.com>
2025-06-23 10:14:03 +0300

    add js-function-syntax

---
+++ 23-06-2025/js-function-syntax.js
@@ -0,0 +1,20 @@
+let add = (num1, num2)=>{
+    return num1 + num2;
+}
+
+let subtract = (num1, num2)=>{
+    return num1 - num2;
+}
+
+let multiply = (num1, num2)=>{
+    return num1 * num2;
+}
+
+let division = (num1, num2)=>{
+    return num1 / num2;
+}
+
+console.log(add(5, 40));
+console.log(subtract(5, 40));
+console.log(multiply(5, 40));
+console.log(division(500, 40));
